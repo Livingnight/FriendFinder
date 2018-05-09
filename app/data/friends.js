61c538c340be6e763f003824734007a7e8cbc67c
unknown <jjernigan1065@gmail.com>
2018-05-08 23:46:55 -0400

    got the modal to work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,82 @@
-$(document).ready( () => {
-    $('select').formSelect();
-
-    $('#surveySubmit').on('click', () => {
-        event.preventDefault();
-        const name = $('#name').val().trim();
-        const picLink = $('#pictureLink').val().trim();
-        const answers = $('select').getSelectedValues();
-        console.log(`Name: ${name}
-        Picture: ${picLink}
-        Answers: ${answers})`)
-    })
-});
+let friends = [
+    {
+        name: "Garland",
+        picture: "k;kljasdf;lja;sklj",
+        answers: [1,
+        4,
+        5,
+        3,
+        4,
+        4,
+        2,
+        4,
+        5,
+        1
+        ]
+    },
+    {
+        name: "Vanessa",
+        picture: "bljsdfl;kjasfd",
+        answers: [
+            4,
+            3,
+            5,
+            2,
+            2,
+            1,
+            3,
+            4,
+            4,
+            6
+        ]
+    },
+    {
+        name: "Garland",
+        picture: "k;kljasdf;lja;sklj",
+        answers: [
+            1,
+            4,
+            5,
+            3,
+            4,
+            4,
+            2,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Garland",
+        picture: "k;kljasdf;lja;sklj",
+        answers: [
+            1,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Garland",
+        picture: "k;kljasdf;lja;sklj",
+        answers: [
+            1,
+            4,
+            5,
+            3,
+            4,
+            4,
+            2,
+            4,
+            5,
+            1
+        ]
+    },
+];
+module.exports = friends;
